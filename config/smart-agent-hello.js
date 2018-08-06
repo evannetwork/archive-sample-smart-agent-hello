5c7bc84afb2addb90f918114fbd70a074eee172b
Jan Schukat <jan.schukat@contractus.com>
2018-08-06 21:04:15 +0200

    licence Apache 2

--- config/smart-agent-hello.js
+++ config/smart-agent-hello.js
@@ -1,3 +1,21 @@
+/*
+  Copyright (c) 2018-present evan GmbH.
+ 
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+      http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  
+*/
+
 exports['default'] = {
   ethAccounts: {
     '0xE3e091EBC4A2474fb45C00B358B3D3ec5b1550AE' : '32cb1206bad10bdd1c009d02ab53b9c7e0d80308e63dbe51a90bc9f8fcb7a9c2',
